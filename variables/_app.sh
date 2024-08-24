c6e1e2e3c2da3c3078ac2e45e98396614db6fce0
ederUp <121892802+ederUp@users.noreply.github.com>
2024-08-24 10:02:44 -0500

    Update email _app.sh

--- variables/_app.sh
+++ variables/_app.sh
@@ -12,4 +12,4 @@ db_pass=$(openssl rand -base64 32)
 db_user=$(openssl rand -base64 32)
 db_name=$(openssl rand -base64 32)
 
-deploy_email=deploy@deploy.com
+deploy_email=sistemas.protecnica@gmail.com
